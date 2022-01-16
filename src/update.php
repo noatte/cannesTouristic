ce3c2b9afece3e39b4ddcc257029449a98c4c7d9
noatte <49617162+noatte@users.noreply.github.com>
2022-01-16 13:22:34 +0100

    Update update.php
    added success or error messages

--- src/update.php
+++ src/update.php
@@ -11,8 +11,17 @@ catch (Exception $e)
         die('Erreur : ' . $e->getMessage());
 }
 
+try
+{
 $markersStatement = $db->prepare('UPDATE `markers` SET `checked` = '. $checked .' WHERE `markers`.`id` = ' . $id);
 $markersStatement->execute();
+echo("Your history has been saved");
+}
+catch (Exeption $e)
+{
+echo("Error : ".$e);
+}
+
 
 
 
